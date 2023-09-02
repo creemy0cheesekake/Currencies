b2e97e6250d4940d28b757dd7984d695f29d9f9b
creemy0cheesekake <arjunanand1405@gmail.com>
2023-09-01 22:30:39 -0500

    getting it working again

--- server/index.ts
+++ server/index.ts
@@ -3,12 +3,16 @@ const app = express();
 import router from "./routes/routes";
 let cors = require("cors");
 
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+});
+app.use(cors({origin: true, credentials: true}));
 
 app.use("/api/v1", router);
 
-if (process.env.NODE_ENV === "production")
-	app.use(express.static("../client/build/"));
+app.use(express.static("../client/build/"));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,10 +2,10 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const controllers_1 = require("../controllers/controllers");
-const router = express_1.default.Router();
+exports.__esModule = true;
+var express_1 = __importDefault(require("express"));
+var controllers_1 = require("../controllers/controllers");
+var router = express_1["default"].Router();
 router.route("/").get(controllers_1.getCurrentData);
 router.route("/:query").get(controllers_1.getDataFromSearch);
-exports.default = router;
+exports["default"] = router;
